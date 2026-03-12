376bbd5ba2072a280c0f85d8430a0db3e38aeb16
OrestesNZ <74434078+OrestesNZ@users.noreply.github.com>
2026-03-12 21:37:11 +1300

    Initial commit

--- HorseBoy.py
+++ HorseBoy.py
@@ -4,9 +4,31 @@
 # git commit -m "Initial commit"
 # git push
 
+# branching - create wokring on a safe copy 
+# parellel 
+# git checkout -b feature-test 
+
+# check which branch you are on 
+# git branch
+
+#go back to main
+# git checkout main
+
+# updating 
+# pull the changes down - make a change on the github (friend changes), local code will be out of date, need to pull down the changes to update local code
+# git pull origin main 
+
+# collaborating 
+# grant them permission
+# they clone, they edit, they push, sync
+
+# merge conflicts - when two people edit the same line of code, git doesn't know which one to keep, need to resolve the conflict manually
+
 print("horse cum contains macro and micro nutrients")
 
 if 2 * 2 != 5:
     print("horse cum is not 5")
 else:
     print("horse cum is 5")
+
+print("aids")
